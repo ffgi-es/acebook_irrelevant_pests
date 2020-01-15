9c1f5fa158b330f2acc3305197c0601a824da3fc
Sam Folorunsho <55720124+samfolo@users.noreply.github.com>
2020-01-15 16:15:42 +0000

    Sign in (#23)
    * fix: switch config.assets.compile to true
    
    * fix: issue solved, safety commit
    
    * feat: log in and log out
    
    * feat: log in and log out

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,23 @@
+require_relative 'web_helpers'
+
+RSpec.describe 'Logging in', type: :feature do
+  scenario 'a user Logs in' do
+    sign_up
+
+    visit '/welcome'
+    click_on 'Log in'
+    fill_in 'email', with: "bean@gmail.com"
+    fill_in 'password', with: '1234password'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Welcome heen!'
+  end
+
+  scenario 'a user Logs out' do
+    sign_up
+    log_in
+    click_on 'Log out'
+
+    expect(page).to have_content 'Welcome to Acebook'
+  end
+end
