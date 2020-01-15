fe255adcf1e9937e88bf7e6530c99f3783e3c85c
Kealan Heena <kealanheena@gmail.com>
2020-01-15 16:39:45 +0000

    remove 'p's

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   
   def new
-    p "New: #{params}"
     @post = Post.new
   end
 
   def create
-    # p "Create: #{params}"
     @post = Post.create(post_params)
     redirect_to posts_url
   end
@@ -27,13 +25,11 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    # p "Index: #{params}"
   end
 
   private
 
   def post_params
-    # p "Post params #{params}"
     params.require(:post).permit(:message)
   end
 
