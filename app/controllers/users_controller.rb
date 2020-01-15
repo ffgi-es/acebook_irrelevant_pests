11cb241d72fb8dd43752998bff834be4cb93607d
Kealan Heena <kealanheena@gmail.com>
2020-01-15 16:40:17 +0000

    remove 'p's

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      p params
       user = User.find_by({ email: params[:email] })
       redirect_to "/users/#{user.id}" if user
     else
