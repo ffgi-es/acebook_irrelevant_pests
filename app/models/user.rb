e10f32fcb72e97a8387b51ff4d7a642ef25febe0
Alastair Gilles <55189413+ffgi-es@users.noreply.github.com>
2020-01-16 18:43:22 +0100

    Email validation (#30)
    * feat: add email validation upon user sign up
    
    * refactor: make rubocop stop complaining

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, 
+            format: { with: /\A([A-z0-9]+)(\.[A-z0-9]+|-[A-z0-9]+)*
+                      @([A-z0-9]+)(\.[A-z0-9]+|-[A-z09]+)*\.[A-z]{2,3}/x }
+
   has_secure_password
 end
