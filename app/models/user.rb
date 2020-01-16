2f9634f1d8076e1cdc72de37bbe27305133a34b9
Alastair Gilles <agilles91@gmail.com>
2020-01-16 17:09:25 +0100

    refactor: make rubocop stop complaining

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, 
-    format: { with: /\A([A-z0-9]+)(\.[A-z0-9]+|-[A-z0-9]+)*@([A-z0-9]+)(\.[A-z0-9]+|-[A-z09]+)*\.[A-z]{2,3}/ }
+            format: { with: /\A([A-z0-9]+)(\.[A-z0-9]+|-[A-z0-9]+)*
+                      @([A-z0-9]+)(\.[A-z0-9]+|-[A-z09]+)*\.[A-z]{2,3}/x }
 
   has_secure_password
 end
