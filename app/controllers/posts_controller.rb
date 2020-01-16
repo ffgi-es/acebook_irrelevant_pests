355d3d358fa4248a795b18502fd882254304a96e
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-16 17:05:07 +0000

    feat: users can only edit their own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,12 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    if session[:id].to_i != @post.user_id
+      edit_error(@post)
+      redirect_to '/posts'
+    else
+      reset_edit_error
+    end
   end
 
   def update
@@ -32,5 +38,19 @@ class PostsController < ApplicationController
   def index
     @user = User.find(session[:id])
     @posts = Post.all
+    @edit_error = session[:invalid_edit]
+    @edit_error_id = session[:invalid_edit_id]
+  end
+
+  private
+
+  def edit_error(post)
+    session[:invalid_edit] = true
+    session[:invalid_edit_id] = post.id.to_s
+  end
+
+  def reset_edit_error
+    session[:invalid_edit] = nil
+    session[:invalid_edit_id] = nil
   end
 end
