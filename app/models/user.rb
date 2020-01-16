e2b1f8a0e4e03a24dde17e8b3c57166c13980429
Jonathan Palma <jonathanpalma89@gmail.com>
2020-01-16 16:36:44 +0000

    Feat: Add password validation and change password fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :email, 
             format: { with: /\A([A-z0-9]+)(\.[A-z0-9]+|-[A-z0-9]+)*
                       @([A-z0-9]+)(\.[A-z0-9]+|-[A-z09]+)*\.[A-z]{2,3}/x }
+  validates :password, length: { in: 6..10 }
 
   has_secure_password
 end
