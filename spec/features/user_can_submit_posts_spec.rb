58cd800feae8242fc4c3e13354d49ca44d60b42a
Hisham Bokhari <hish.b92@gmail.com>
2020-01-17 15:46:32 +0000

    hisham

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -4,13 +4,17 @@ require_relative 'web_helpers'
 RSpec.feature "Timeline", type: :feature do
   before do
     sign_up
-  end
-  
-  scenario "Can submit posts and view them" do
-    sign_up
     click_on 'New post'
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
+  end
+  
+  scenario "Can submit posts and view them" do
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario 'Users name appears on the post' do
+    expect(page).to have_content "Posted by heen"
+  end
+
 end
