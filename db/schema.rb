1c7c187b14f4b095defe9e94c45b24d8141ae6bc
Jonathan Palma <jonathanpalma89@gmail.com>
2020-01-20 15:25:42 +0000

    feat: add user wall posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200117162321) do
+ActiveRecord::Schema.define(version: 20200120143354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200117162321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "wall_user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
---
+++ spec/features/posting_to_another_users_wall_spec.rb
@@ -0,0 +1,21 @@
+describe 'Posting to another users wall', type: :feature do
+  it 'display the posts only on their wall' do
+    user = User.create(first_name: 'Alastair',
+                       last_name: 'Blam',
+                       email: 'alastair@example.com',
+                       password: '123456')
+    sign_up
+    visit user_path(user)
+    click_on 'New post'
+
+    fill_in 'Message', with: 'Hello!'
+    click_on 'Submit'
+
+    expect(page).to have_current_path user_path(user)
+    expect(page).to have_content 'Hello!'
+
+    visit posts_path
+
+    expect(page).not_to have_content 'Hello!'
+  end
+end
