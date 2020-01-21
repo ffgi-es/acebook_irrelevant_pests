96177e2bfe8cc5cb65dbacee760fc35137b31bfd
kealanheena <kealanheena@gmail.com>
2020-01-21 16:26:06 +0000

    fix: make user page post appear in order of most frequent

--- app/assets/javascripts/konami.js
+++ app/assets/javascripts/konami.js
@@ -11,9 +11,25 @@ function onKonamiCode(assertion) {
   });
 }
 
+// animation for konami code
+$(document).keydown(function(e) {
+  if(e.which === 65 ||
+     e.which === 76 ||
+     e.which === 65 ||
+     e.which === 83 ||
+     e.which === 84 ||
+     e.which === 65 ||
+     e.which === 73 ||
+     e.which === 82 ||
+     ) {
+    alert("Gotcha!!")
+  }
+  e.preventDefault();
+});
+
 onKonamiCode(function() {
   var element = document.getElementById("konami");
   window.scrollTo(0, 0);
   element.innerHTML = '<img src="https://i.imgflip.com/1jng63.jpg" class="ui fluid image">'
-  alert("FAKE NEWS!!!");
+  // alert("FAKE NEWS!!!");
 });
