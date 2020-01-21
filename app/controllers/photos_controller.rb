d078cd3cd2624354268b1fbb372cf0a31c43ef74
Hisham Bokhari <hish.b92@gmail.com>
2020-01-21 12:33:01 +0000

    Feat: Can't delete other users photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :authorize, only: [:edit, :update]
+  before_action :authorize, only: [:edit, :update, :destroy]
 
   def new
   end
@@ -25,6 +25,11 @@ class PhotosController < ApplicationController
     redirect_to session.delete(:return_to)
   end
 
+  def destroy
+    Photo.delete(params[:id])
+    redirect_to request.referer
+  end
+
   private
 
   def photo_params
@@ -37,7 +42,7 @@ class PhotosController < ApplicationController
 
   def authorize
     return if session[:id] == Photo.find(params[:id]).user_id
-    
+
     session[:photo_edit_error] = "You cannot edit other peoples photos" 
     redirect_to request.referer
   end
